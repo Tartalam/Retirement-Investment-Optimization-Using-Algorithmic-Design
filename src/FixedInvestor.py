e8493090b1bdd6c993d63169b9f7a598844259bb
Justin Powell <powell.oj.justin@gmail.com>
2025-12-01 23:53:07 -0500

    Created data file to store setters and getters for shared variables

--- src/FixedInvestor.py
+++ src/FixedInvestor.py
@@ -1,10 +1,14 @@
+from Data import data
+
 class FixedInvestor:
-    def __init__(self, principal, years, rate):
-        self.principal = float(principal)
-        self.years = int(years)
-        self.rate = float(rate)
+    def __init__(self):
+        self.principal = data.principal
+        self.years = data.years
+        self.rate = data.rate
 
     @classmethod
     # Simulates the compound growth of retirement savings over a fixed period of time and interest rate.
     def fixed_investor(cls, principal, years, rate):
-        return principal * (1 + rate) ** years # Formula for compound interest 
+        balance = principal * (1 + rate) ** years # Formula for compound interest 
+        data.balance = balance
+        return balance 
