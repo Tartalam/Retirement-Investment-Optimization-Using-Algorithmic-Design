372a2680328a5fa4c91e809377d68e7c74383d7c
Raman Watson <154751732+Watodon@users.noreply.github.com>
2025-12-03 20:37:36 -0500

    Add metadata comments to VariableInvestor.py
    Added author, date, ID, and description comments to VariableInvestor.py

--- src/VariableInvestor.py
+++ src/VariableInvestor.py
@@ -1,3 +1,9 @@
+#Author: Raman Watson
+#Date: 03/12/2025
+#ID: 1701474
+#Description: Calculates investment growth with variable interest rates over multiple periods
+#Atrributions: AI was used to provide a more efficient algorithm, weeding out the unnecessary code I had.
+
 class VariableInvestor:
     """Utility class for variable rate calculations."""
     
